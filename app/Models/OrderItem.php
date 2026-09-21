b248df556b7db3cbda0c61110b399d29b9776eda
Ana Luiza <analuizasvasconcelos3@gmail.com>
2026-09-21 11:36:16 -0300

    Add OrderItem model

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id', 'product_id', 'quantity',
+        'unit_price', 'unit_cost', 'total_price', 'total_profit',
+    ];
+
+    protected $casts = [
+        'unit_price' => 'decimal:2',
+        'unit_cost' => 'decimal:2',
+        'total_price' => 'decimal:2',
+        'total_profit' => 'decimal:2',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
